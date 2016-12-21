8c748bca88d11ed95111e4ffa9a171c393af2dd0
Douglas Navarro <Douglas Navarro>
2016-12-21 12:17:59 -0200

    adicionando Ex1: primeiro programa com ESP8266

---
+++ Ex1.ino
@@ -0,0 +1,18 @@
+/*
+  www.CodeDecay.com.br
+  Licença de uso: Apache 2.0
+
+  Neste sketch inicializamos a comunicação 
+  serial com o ESP, enviamos comandos
+  e observamos as respostas.
+ */
+
+void setup()
+{
+  
+}
+ 
+void loop()
+{
+   
+}
